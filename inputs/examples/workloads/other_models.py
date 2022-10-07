777e3d4d2276a6d4806f89cb4bdd8e2571e5e76b
Patrick McEwen <pmcewen@stanford.edu>
2022-10-07 09:49:00 -0700

    Delete other_models.py

--- inputs/examples/workloads/other_models.py
+++
@@ -1,6 +0,0 @@
-import onnx
-from onnx import shape_inference
-
-model = onnx.load('/Downloads/onnx_models/resnet101-v1-7.onnx')
-inferred_model = shape_inference.infer_shapes(model)
-onnx.save(inferred_model, "/Downloads/onnx_models/resnet101-v1-7.onnx")
