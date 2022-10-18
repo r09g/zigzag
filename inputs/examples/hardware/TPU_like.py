af2a30c9c7baebfe4e61a3f5d937bc9f7e230896
asyms <asymons@micaszb03.esat.kuleuven.be>
2022-10-18 14:27:51 -0700

    fix hw iterator stage by recalculating core memory attributes after memory hierarchy update

--- inputs/examples/hardware/TPU_like.py
+++ inputs/examples/hardware/TPU_like.py
@@ -15,7 +15,7 @@ def memory_hierarchy_dut(multiplier_array):
     reg_W_128B = MemoryInstance(name="rf_128B", size=128 * 8, r_bw=8, w_bw=8, r_cost=0.095, w_cost=0.095, area=0,
                                 r_port=1, w_port=1, rw_port=0, latency=1) # 128 B
 
-    reg_O_1K = MemoryInstance(name="rf_1KB", size=128, r_bw=16, w_bw=16, r_cost=0.54, w_cost=0.6, area=0,
+    reg_O_1K = MemoryInstance(name="rf_2B", size=16, r_bw=16, w_bw=16, r_cost=0.54, w_cost=0.6, area=0,
                               r_port=2, w_port=2, rw_port=0, latency=1) # 2 B
 
     # reg_W_128B = MemoryInstance(name="rf_128B", size=128 * 8, r_bw=8, w_bw=8, r_cost=9.5, w_cost=9.5, area=0,
